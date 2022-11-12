aa50b2e1299c72df6572605da7e218068b085d04
Saransh Chopra <saransh0701@gmail.com>
2022-11-12 19:41:02 +0530

    Minimalise

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from glob import glob
 
 example_module = Extension('_casadi',
                            sources=['swig/casadi.i'],
-                           depends=['casadi/casadi.hpp'],
                            include_dirs=['.'],
                            swig_opts=['-c++', '-python', '-py3'],
 )
@@ -16,6 +15,4 @@ setup(
    description = """Simple swig example from docs""",
    ext_modules = [example_module],
    py_modules = ["casadi"],
-   headers=['casadi/casadi.hpp'],
-   extra_compile_args=["-I."]
 )
