90c9e40da0eba7184ba7079f138a43d8671bcd55
Saransh Chopra <saransh0701@gmail.com>
2022-11-12 15:12:44 +0530

    Try adding headers

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ example_module = Extension('_casadi',
                            sources=['swig/casadi_wrap.cxx'], include_dirs=['./casadi/*']
                            )
 
-setup (name = 'casadi',
-       version = '0.1',
-       author      = "SWIG Docs",
-       description = """Simple swig example from docs""",
-       ext_modules = [example_module],
-       py_modules = ["casadi"],
-    #    headers=['casadi/core/casadi_interrupt.hpp', 'casadi/casadi.hpp']
-       )
+setup(
+   name = 'casadi',
+   version = '0.1',
+   author      = "SWIG Docs",
+   description = """Simple swig example from docs""",
+   ext_modules = [example_module],
+   py_modules = ["casadi"],
+   headers=['casadi/core/casadi_interrupt.hpp', 'casadi/casadi.hpp']
+)
